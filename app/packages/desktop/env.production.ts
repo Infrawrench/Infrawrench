26a17cda6c8f0b242e6d0988e40e8280c123e880
Astrid Gealer <astrid@gealer.email>
2026-07-24 11:20:32 +0100

    chore: Update production env for desktop app.

--- app/packages/desktop/env.production.ts
+++ app/packages/desktop/env.production.ts
@@ -1,9 +1,9 @@
 export const PROTOCOL = "infrawrench";
-export const CLIENT_ID = "client_01KNCYFN2YP2RVC0S2HR63EQVC";
+export const CLIENT_ID = "client_01KY8MAZ7NCYMN6K44G9FFZ5KS";
 export const CLOUD_URL = "https://web.infrawrench.com";
 export const TELEMETRY_URL = "https://telemetry.infrawrench.com";
 export const WORKOS_API_URL = "https://api.workos.com";
-export const SHOW_SIGN_IN_BUTTON = false;
+export const SHOW_SIGN_IN_BUTTON = true;
 export const BANNERS: { message: string; variant?: "info" | "warning" }[] = [
   {
     message: "Infrawrench is in alpha. Expect some errors and missing functionality.",
